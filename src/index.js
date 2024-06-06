d6e5553e0409ab393ba86bb09035c5d755c4db79
alexanderjess <alexanderj2@pella.com>
2024-06-06 16:33:23 -0500

    further setup to align with jest config

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import {mapWithCb} from './mapWithCb';
+
+/*
+Your task is to write a function, fizzBuzz, that accepts a number and returns a string:
+
+'fizz' if the number is divisible by 3;
+'buzz' if the number is divisible by 5;
+'fizzbuzz' if the number is divisible by both 3 and 5.
+'{number}' if the number doesn't fulfil any of the above conditions.
+As you create the function in fizzbuzz.js, you should also write tests in index.test.js. You'll need to create your own test cases as you go.
+
+Edge cases to consider:
+
+what should fizzbuzz(0) return?
+what should happen if the function is passed a string?
+
+ */
+const fizzBuzz = () => {
+  return 3;
+};
+
+export {
+  fizzBuzz,
+  mapWithCb
+};
---
+++ test/index.test.js
@@ -0,0 +1,48 @@
+/*
+;
+
+'fizzbuzz' if the number is divisible by both 3 and 5.
+'{number}' if the number doesn't fulfil any of the above conditions.
+ */
+import {fizzBuzz} from "../src";
+
+describe('1. fizzBuzz', () => {
+  it('shows \'fizz\' if the number is divisible by 3', () => {
+    const result = fizzBuzz(3);
+    expect(result).toBe(3);
+  });
+});
+
+describe('2. mapWithCb', () => {
+  it('throws an error if first argument is not an array', () => {
+
+  });
+
+  it('throws an error if second argument is not a function', () => {
+
+  });
+
+  it('calls the given function at least once', () => {
+
+  });
+
+  it('calls the given function a number of times equal to the length of the given array', () => {
+
+  });
+
+  it('calls the given function with any one item from the given array', () => {
+
+  });
+
+  it('calls the given function a second time with the second item in the given array', () => {
+
+  });
+
+  it('calls the given function a final time with the final item in the given array', () => {
+
+  });
+})
+
+describe('3. getPokemon', () => {
+
+});
